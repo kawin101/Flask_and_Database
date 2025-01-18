691fe78c974909befd35bd057506d50348f83406
Kawinphop Chomnikorn <kawinphop.chomnikorn@gmail.com>
2025-01-18 17:59:14 +0700

    run-the-project-macos-15.1

--- app.py
+++ app.py
@@ -1,38 +1,40 @@
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
-app=Flask(__name__)
+app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mystatement.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 class Statement(db.Model):
-    id = db.Column(db.Integer,primary_key=True)
-    date = db.Column(db.String(50),nullable=False)
-    name = db.Column(db.String(100),nullable=False)
-    amount = db.Column(db.Integer,nullable=False)
-    category = db.Column(db.String(50),nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.String(50), nullable=False)
+    name = db.Column(db.String(100), nullable=False)
+    amount = db.Column(db.Integer, nullable=False)
+    category = db.Column(db.String(50), nullable=False)
 
 with app.app_context():
     db.create_all()
 
 @app.template_filter()
 def currencyFormat(value):
-    value=float(value)
+    value = float(value)
     return "{:,.2f}".format(value)
 
 @app.route("/addForm")
 def addForm():
     return render_template("addForm.html")
 
-@app.route("/addStatement",methods=['POST'])
+@app.route("/addStatement", methods=['POST'])
 def addStatement():
     date = request.form["date"]
     name = request.form["name"]
     amount = request.form["amount"]
     category = request.form["category"]
-    statement=Statement(date=date,name=name,amount=amount,category=category)
+    statement = Statement(date=date, name=name, amount=amount, category=category)
     db.session.add(statement)
     db.session.commit()
     return redirect("/")
@@ -40,7 +42,7 @@ def addStatement():
 @app.route("/")
 def showData():
     statements = Statement.query.all()
-    return render_template("statements.html",statements=statements)
+    return render_template("statements.html", statements=statements)
 
 @app.route("/delete/<int:id>")
 def deleteStatement(id):
@@ -52,9 +54,9 @@ def deleteStatement(id):
 @app.route("/edit/<int:id>")
 def editStatement(id):
     statement = Statement.query.filter_by(id=id).first()
-    return render_template("editForm.html",statement=statement)
+    return render_template("editForm.html", statement=statement)
 
-@app.route("/updateStatement",methods=['POST'])
+@app.route("/updateStatement", methods=['POST'])
 def updateStatement():
     id = request.form["id"]
     date = request.form["date"]
